7f4490251bf4cabd07cc03134e372bb2c608b0da
Juan Sequeiros <juan.sequeiros@optimissa.com>
2017-01-26 18:06:43 +0100

    Spot is last working day of month rule

--- datecalc-jdk8/src/main/java/net/objectlab/kit/datecalc/jdk8/LocalDateCurrencyDateCalculator.java
+++ datecalc-jdk8/src/main/java/net/objectlab/kit/datecalc/jdk8/LocalDateCurrencyDateCalculator.java
@@ -1,8 +1,11 @@
 package net.objectlab.kit.datecalc.jdk8;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.temporal.TemporalAdjusters;
 
 import net.objectlab.kit.datecalc.common.CurrencyDateCalculatorBuilder;
+import net.objectlab.kit.datecalc.common.NonWorkingDayChecker;
 import net.objectlab.kit.datecalc.common.ccy.AbstractCurrencyDateCalculator;
 
 /**
@@ -36,4 +39,29 @@ public class LocalDateCurrencyDateCalculator extends AbstractCurrencyDateCalcula
         return d1.isAfter(d2) ? d1 : d2;
     }
 
+    @Override
+    protected LocalDate lastDayOfMonth(LocalDate date) {
+        return date.with(TemporalAdjusters.lastDayOfMonth());
+    }
+
+    @Override
+    public LocalDate adjustDate(LocalDate startDate, int increment, NonWorkingDayChecker<LocalDate> checker) {
+        LocalDate date = startDate;
+        final int month = date.getMonthValue();
+        int stepToUse = increment;
+        while (checker.isNonWorkingDay(date)) {
+            date = date.plusDays(stepToUse);
+            if (date.getMonthValue() != month) {
+                // flick to backward
+                stepToUse *= -1;
+                date = date.plusDays(stepToUse);
+            }
+        }
+        return date;
+    }
+
+    @Override
+    protected Boolean equal(LocalDate d1, LocalDate d2) {
+        return d1.isEqual(d2);
+    }
 }
